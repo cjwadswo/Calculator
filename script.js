0ef6b775e492605e9b39b2acec1dfdbd1f468f47
Chris Wadsworth <cjwadswo@uno.edu>
2022-09-12 03:43:24 -0500

    Refactor callback function for number button event
    Created a function for the callback function for the event listener
    instead of having an arrow function. This will allow for the reuse of
    the function for when keyboard input is implemented.

--- script.js
+++ script.js
@@ -2,29 +2,39 @@ const display = document.getElementById("display");
 let leftOperand = 0;
 let rightOperand = 0;
 let operand = null;
+
+let additionButton = document.getElementById("add");
+let subtractionButton = document.getElementById("subtract");
+let multiplicationButton = document.getElementById("multiply");
+let divisionButton = document.getElementById("divide");
 //This function adds event listeners on the number buttons 
 function initalizeNumberButtons() {
-    let buttonNum;
+    let numberButton;
+    //for loop is going to loop 10 times for the 10 number buttons.
     for(let i = 0; i < 10; i++) {
-        buttonNum = document.querySelector(`#num${i}`);
-        buttonNum.addEventListener('click', e => {
-            if(operand === null){
-                leftOperand = e.target.textContent;
-                display.textContent += leftOperand;
-                console.log(e.target.textContent);
-            }
-            else{
-                leftOperand = e.target.textContent;
-                display.textContent += num1;
-            }
-        })  
+        //buttonNum is going to hold the value of the css element with the ID num${i}
+        numberButton = document.querySelector(`#num${i}`);
+        //Add the event listener to these number buttons
+        numberButton.addEventListener('click', updateDisplay);  
     } 
 }
 
+function updateDisplay(e){
+    if(operand === null){
+        leftOperand = e.target.textContent;
+        display.textContent += leftOperand;
+        console.log(e.target.textContent);
+    }
+    else{
+        leftOperand = e.target.textContent;
+        display.textContent += num1;
+    }
+}
 //This function
 function initializeOperatorButtons() {
+    
     //Addition event listener
-
+    additionButton.addEventListener('click')
     //Subtraction event listener
 
     //Multiplication event listener
